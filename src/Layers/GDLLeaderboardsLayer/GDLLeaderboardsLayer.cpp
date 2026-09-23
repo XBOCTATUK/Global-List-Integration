63fc6ebe1ae0ad9c7d571475c7a3fe2cff1a8e49
XBOCTATUK <xboctatuk213@gmail.com>
2026-09-23 16:58:41 +0700

    faqing android, whats wrong?

--- src/Layers/GDLLeaderboardsLayer/GDLLeaderboardsLayer.cpp
+++ src/Layers/GDLLeaderboardsLayer/GDLLeaderboardsLayer.cpp
@@ -219,17 +219,17 @@ bool GDLLeaderboardsLayer::init() {
 		}
 	};
 
-	m_userLeaderboardLoadListener = UserLeaderboardLoadedEvent().listen(
-		userLeaderboardCallback
-	);
+	// m_userLeaderboardLoadListener = UserLeaderboardLoadedEvent().listen(
+	// 	userLeaderboardCallback
+	// );
 
-	m_countryMainLeaderboardLoadListener = CountryLeaderboardLoadedEvent(CountriesLeaderboardType::Main).listen(
-		countryLeaderboardCallback
-	);
+	// m_countryMainLeaderboardLoadListener = CountryLeaderboardLoadedEvent(CountriesLeaderboardType::Main).listen(
+	// 	countryLeaderboardCallback
+	// );
 
-	m_countryAdvancedLeaderboardLoadListener = CountryLeaderboardLoadedEvent(CountriesLeaderboardType::Advanced).listen(
-		countryLeaderboardCallback
-	);
+	// m_countryAdvancedLeaderboardLoadListener = CountryLeaderboardLoadedEvent(CountriesLeaderboardType::Advanced).listen(
+	// 	countryLeaderboardCallback
+	// );
 
 	showLoading();
 	GDL::API::Leaderboards::getUserLeaderboard();
@@ -239,7 +239,7 @@ bool GDLLeaderboardsLayer::init() {
 
 void GDLLeaderboardsLayer::onTabButton(cocos2d::CCObject* sender) {
 	m_searchBar->getDropdownList()->setOpen(false);
-	
+
 	if (sender->getTag() == static_cast<int>(LeaderboardsType::Players) && m_type != LeaderboardsType::Players) {
 		m_countriesTabBtn->toggle(false);
 		m_type = LeaderboardsType::Players;
