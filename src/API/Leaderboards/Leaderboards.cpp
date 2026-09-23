f0eac8bbe799bc1271aa386fd3e87bf5e74438f8
XBOCTATUK <xboctatuk213@gmail.com>
2026-09-23 18:09:23 +0700

    Какие пиздатые у меня названия коммитов, я не могу)

--- src/API/Leaderboards/Leaderboards.cpp
+++ src/API/Leaderboards/Leaderboards.cpp
@@ -70,7 +70,8 @@ namespace GDL::API::Leaderboards {
                 
                 GDL::Cache::Leaderboards::setUserLeaderboard(key, std::move(userIDs));
                 auto cachedUsers = GDL::Cache::Leaderboards::getUserLeaderboard(key);
-                log::info("{} | first: {}", !!cachedUsers, cachedUsers->at(0));
+                log::info("{}", !!cachedUsers);
+                log::info("first: {}", cachedUsers->at(0));
                 // UserLeaderboardLoadedEvent().send(
                 //     Ok(*cachedUsers)
                 // );
