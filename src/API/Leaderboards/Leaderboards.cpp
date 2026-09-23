feadfc761f074262a33937ff78420d7aac6307c9
XBOCTATUK <xboctatuk213@gmail.com>
2026-09-23 17:44:36 +0700

    please, don't crash again

--- src/API/Leaderboards/Leaderboards.cpp
+++ src/API/Leaderboards/Leaderboards.cpp
@@ -70,9 +70,9 @@ namespace GDL::API::Leaderboards {
                 
                 GDL::Cache::Leaderboards::setUserLeaderboard(key, std::move(userIDs));
                 auto cachedUsers = GDL::Cache::Leaderboards::getUserLeaderboard(key);
-                // UserLeaderboardLoadedEvent().send(
-                //     Ok(*cachedUsers)
-                // );
+                UserLeaderboardLoadedEvent().send(
+                    Ok(*cachedUsers)
+                );
             }
         );
     }
