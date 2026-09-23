46ce8dd2bd1645228b148945f32c6b28c57d1b4a
XBOCTATUK <xboctatuk213@gmail.com>
2026-09-23 17:33:42 +0700

    Okay, let's analyze it!

--- src/API/Leaderboards/Leaderboards.cpp
+++ src/API/Leaderboards/Leaderboards.cpp
@@ -69,8 +69,9 @@ namespace GDL::API::Leaderboards {
                 }
                 
                 GDL::Cache::Leaderboards::setUserLeaderboard(key, std::move(userIDs));
+                auto cachedUsers = GDL::Cache::Leaderboards::getUserLeaderboard(key);
                 // UserLeaderboardLoadedEvent().send(
-                //     Ok(*GDL::Cache::Leaderboards::getUserLeaderboard(key))
+                //     Ok(*cachedUsers)
                 // );
             }
         );
